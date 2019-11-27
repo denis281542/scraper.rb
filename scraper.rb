7aa305c83c740b50d00560e57c4b388664880fbc
denis281542 <deniskuimov@mail.ru>
2019-11-27 09:47:16 +0500

    Create iterator

--- scraper.rb
+++ scraper.rb
@@ -6,8 +6,10 @@ def scraper
   url = "http://vsalde.ru/"
   unparsed_page =  HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
-  news_listings = parsed_page.css('div.news-main')
-
+  news_listings = parsed_page.css('div.news-main') # 19 news
+  news_listings.each do |news_listing|
+    
+  end
   byebug
 end
 
