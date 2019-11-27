cc4bf1fb6220fb778c8ec83fe62593c993b8587b
denis281542 <deniskuimov@mail.ru>
2019-11-27 09:44:45 +0500

    Added news listing

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,8 @@ def scraper
   url = "http://vsalde.ru/"
   unparsed_page =  HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
+  news_listings = parsed_page.css('div.news-main')
+
   byebug
 end
 
