05b1b652ad69415701e92006df6c0b16d1a02bb7
denis281542 <deniskuimov@mail.ru>
2019-11-28 23:53:08 +0500

    Comment

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 # Fetch and parse HTML document
-doc = Nokogiri::HTML(open('https://www.mvideo.ru/product-list-page-cls?q=macbook&region_id=5&limit=12&category_id=cat2_cis_0000000130'))
+doc = Nokogiri::HTML(open('0130'))
 
 puts "div.product-price"
 doc.css('div.product-price').each do |link|
