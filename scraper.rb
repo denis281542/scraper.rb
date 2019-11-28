244a3cc03a3957624e29922f8e8a7b8d3059a99d
denis281542 <deniskuimov@mail.ru>
2019-11-28 23:53:53 +0500

    Comment

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 # Fetch and parse HTML document
-doc = Nokogiri::HTML(open('yandex.com'))
+doc = Nokogiri::HTML(open('yandex.corg'))
 
 puts "div.product-price"
 doc.css('div.product-price').each do |link|
