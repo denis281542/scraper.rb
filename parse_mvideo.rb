4a48d8d14bda032d7f755ce9e04af34881ac55d2
denis281542 <deniskuimov@mail.ru>
2019-11-30 21:43:23 +0500

    Edit scraper

---
+++ parse_mvideo.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+url = "https://www.mvideo.ru/product-list-page-cls?q=macbook&region_id=5&limit=12&category_id=cat2_cis_0000000130"
+doc = Nokogiri::HTML(open(url))
+puts  doc.css(".search-results-cluster-subtitle_step2").text
+=begin
+doc.css(".u-size-1-5-xl").each do |items|
+  title = items.css(".line-clamp-2").text
+  price = items.css(".price-main").text[/[0-9\.]+/]
+  puts  "#{title} - #{price}"
+  #puts items.at_css(".line-clamp-2")[:href]
+end
+=end
+
+
+ 
