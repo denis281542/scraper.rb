6b981920e6c84df985b7f2d50931ccda5db4f8e9
denis281542 <deniskuimov@mail.ru>
2019-12-01 20:55:44 +0500

    Added salary

--- neuvoo.rb
+++ neuvoo.rb
@@ -4,4 +4,8 @@ require 'open-uri'
 
 url = "https://ekaterinburg.hh.ru/search/vacancy?area=3&st=searchVacancy&text=ruby+on+rails"
 doc = Nokogiri::HTML(open(url))
-puts  doc.css(".header").text
+puts  doc.at_css(".header").text
+puts  doc.at_css(".HH-LinkModifier").text #vakancy
+puts  doc.at_css(".vacancy-serp-item__compensation").text #salary
+
+
