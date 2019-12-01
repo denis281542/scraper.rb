038bb643ba6c26aa47eea3be49de98f540246bb7
denis281542 <deniskuimov@mail.ru>
2019-12-01 20:52:01 +0500

    First commit

--- parse_mvideo.rb
+++ parse_mvideo.rb
@@ -5,7 +5,6 @@ require 'open-uri'
 url = "https://www.mvideo.ru/product-list-page-cls?q=macbook&region_id=5&limit=12&category_id=cat2_cis_0000000130"
 doc = Nokogiri::HTML(open(url))
 puts  doc.css(".search-results-cluster-subtitle_step2").text
-
 doc.css(".showcompare").each do |items|
   title = items.css(".product-tile-title").text
   price = items.css(".sel-product-tile-price").text[/[0-9\.]+/]
