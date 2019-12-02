b2d5fba5f0d8a1740a9b33c27f59e7e13b141c8e
denis281542 <deniskuimov@mail.ru>
2019-12-02 18:46:04 +0500

    Added vacansy in item

--- neuvoo.rb
+++ neuvoo.rb
@@ -4,12 +4,20 @@ require 'open-uri'
 
 url = "https://ekaterinburg.hh.ru/search/vacancy?area=3&st=searchVacancy&text=ruby+on+rails"
 doc = Nokogiri::HTML(open(url))
+puts  doc.css(".vacancy-serp-item").text
+
+
+
+
+
+
+=begin
 puts  doc.at_css(".header").text
 puts  doc.at_css(".HH-LinkModifier").text #vakancy
 puts  doc.at_css(".vacancy-serp-item__compensation").text #salary
 puts  doc.at_css(".HH-AnonymousIndexAnalytics-Recommended-Company").text #company
 puts  doc.at_css(".vacancy-serp-item__publication-date").text #date of publication
-
+=end
 
 
 
