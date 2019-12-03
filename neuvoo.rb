39b97ae389ea5b7e18c4c91ee584d3b6475690da
denis281542 <deniskuimov@mail.ru>
2019-12-03 16:49:50 +0500

    Edit only work with salary

--- neuvoo.rb
+++ neuvoo.rb
@@ -4,7 +4,6 @@ require 'open-uri'
 
 url = "https://ekaterinburg.hh.ru/search/vacancy?area=3&st=searchVacancy&text=ruby+on+rails"
 doc = Nokogiri::HTML(open(url))
-doc.css(".header").text
 doc.css(".vacancy-serp-item").each do |items|
   vakancy = items.css(".HH-LinkModifier").text
   salary = items.css(".vacancy-serp-item__compensation").text
@@ -21,12 +20,6 @@ end
 
 
 
-=begin
-
-puts  doc.at_css(".vacancy-serp-item__compensation").text #salary
-puts  doc.at_css(".HH-AnonymousIndexAnalytics-Recommended-Company").text #company
-puts  doc.at_css(".vacancy-serp-item__publication-date").text #date of publication
-=end
 
 
 
