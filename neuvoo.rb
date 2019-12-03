d6d2eb570bf4ba46c7aacd3539cc6e8cf509d593
denis281542 <deniskuimov@mail.ru>
2019-12-03 15:59:44 +0500

    Edit vacansy in item

--- neuvoo.rb
+++ neuvoo.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 url = "https://ekaterinburg.hh.ru/search/vacancy?area=3&st=searchVacancy&text=ruby+on+rails"
 doc = Nokogiri::HTML(open(url))
 doc.css(".header").text
-doc.css(".vacancy-serp-item__row_header").each do |items|
+doc.css(".vacancy-serp-item").each do |items|
   vakancy = items.css(".HH-LinkModifier").text
   salary = items.css(".vacancy-serp-item__compensation").text 
   puts  "#{vakancy} - #{salary}"
