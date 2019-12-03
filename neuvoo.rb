2a4c500cb0b7bc1c2c52c8775eb2dd4bada17734
denis281542 <deniskuimov@mail.ru>
2019-12-03 16:43:58 +0500

    Edit only work with salary

--- neuvoo.rb
+++ neuvoo.rb
@@ -9,14 +9,11 @@ doc.css(".vacancy-serp-item").each do |items|
   vakancy = items.css(".HH-LinkModifier").text
   salary = items.css(".vacancy-serp-item__compensation").text
   date_of_publication = items.css(".vacancy-serp-item__publication-date").text 
-  
-  if salary.empty? 
-    salary = "unspecified"
-  end
-  
-  puts "#{vakancy} - #{salary}"
-  puts "#{date_of_publication}"
-  puts "======="
+  unless salary.empty? 
+    puts "#{vakancy} - #{salary}" 
+    puts "#{date_of_publication}"
+    puts "======="
+  end  
 end
 
 
