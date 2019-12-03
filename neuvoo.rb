ba34e48f563dbcb7033124704fd468c0a87faac6
denis281542 <deniskuimov@mail.ru>
2019-12-03 16:20:21 +0500

    Added folfer unspecified

--- neuvoo.rb
+++ neuvoo.rb
@@ -8,7 +8,10 @@ doc.css(".header").text
 doc.css(".vacancy-serp-item").each do |items|
   vakancy = items.css(".HH-LinkModifier").text
   salary = items.css(".vacancy-serp-item__compensation").text
-  date_of_publication = items.css(".vacancy-serp-item__publication-date").text #
+  date_of_publication = items.css(".vacancy-serp-item__publication-date").text 
+  if salary.empty? 
+    salary = "unspecified"
+  end
   puts "#{vakancy} - #{salary}"
   puts "#{date_of_publication}"
   puts "======="
