fc95ad50dc525674db8e74fc12c45fef5511e8f7
denis281542 <deniskuimov@mail.ru>
2019-12-04 23:43:52 +0500

    Edit my_circle.rb

--- my_circle.rb
+++ my_circle.rb
@@ -3,3 +3,5 @@ require 'nokogiri'
 require 'open-uri'
 
 url = "https://moikrug.ru/vacancies?utf8=%E2%9C%93&q=Ruby&location=&city_id=&type=all"
+doc = Nokogiri::HTML(open(url))
+puts doc.css(".inner").text
