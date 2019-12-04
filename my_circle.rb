12f832bb1bcdf9ac32916c31622c3fff4d6a187d
denis281542 <deniskuimov@mail.ru>
2019-12-04 23:48:26 +0500

    Edit my_circle.rb

--- my_circle.rb
+++ my_circle.rb
@@ -5,4 +5,5 @@ require 'open-uri'
 url = "https://moikrug.ru/vacancies?utf8=%E2%9C%93&q=Ruby&location=&city_id=&type=all"
 doc = Nokogiri::HTML(open(url))
 #puts doc.css(".inner").text
-puts doc.css(".title").text
+puts doc.css(".title").text #должность
+puts doc.css(".count").text #зарплата
