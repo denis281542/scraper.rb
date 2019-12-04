354a8c2adf8272fe0d23a1a24f9baf382a81904b
denis281542 <deniskuimov@mail.ru>
2019-12-05 00:00:57 +0500

    Edit my_circle.rb

--- my_circle.rb
+++ my_circle.rb
@@ -6,8 +6,11 @@ url = "https://moikrug.ru/vacancies?utf8=%E2%9C%93&q=Ruby&location=&city_id=&typ
 doc = Nokogiri::HTML(open(url))
 doc.css(".inner").each do |items|
   posi = items.css(".title").text 
-  salary = items.css(".count").text 
+  salary = items.css(".count").text
+  date_publ = items.css(".date").text 
   unless salary.empty?
       puts "#{posi} - #{salary}"
+      puts "#{date_publ}"
+      puts "==================="
   end
 end
