9ed3a72e23dd67c1d48256f26a0d93a92ad168a9
denis281542 <deniskuimov@mail.ru>
2019-12-04 23:34:27 +0500

    Edit parser

--- neuvoo.rb
+++ neuvoo.rb
@@ -7,7 +7,7 @@ doc = Nokogiri::HTML(open(url))
 doc.css(".vacancy-serp-item").each do |items|
   vakancy = items.css(".HH-LinkModifier").text
   salary = items.css(".vacancy-serp-item__compensation").text
-  date_of_publication = items.css(".vacancy-serp-item__publication-date").text 
+  date_of_publ= items.css(".vacancy-serp-item__publication-date").text 
   unless salary.empty? 
     puts "#{vakancy} - #{salary}" 
     puts "#{date_of_publication}"
