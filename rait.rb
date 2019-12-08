d9a737d28fbbe6dbdf3815e788b6847b81079658
denis281542 <deniskuimov@mail.ru>
2019-12-08 19:43:45 +0500

    Added scraper rait

--- rait.rb
+++ rait.rb
@@ -5,8 +5,8 @@ require 'open-uri'
 url = "https://raitdostavka.ru/"
 doc = Nokogiri::HTML(open(url))
 doc.css(".inner_wrap").each do |items|
-  title = items.css(".item-title").text
-  price = items.css(".price_value").text
+  title = items.css(".item-title").text.strip
+  price = items.css(".price_value").text.strip
     puts "#{title} - #{price}" 
     puts "======="
 end
