b491c0a36a5c6dc04ad55096e9fc37d5cd3698d7
denis281542 <deniskuimov@mail.ru>
2020-01-22 23:03:22 +0500

    Edit scraper

--- mechanize.rb
+++ mechanize.rb
@@ -2,8 +2,15 @@ require 'rubygems'
 require 'mechanize'
 
 agent = Mechanize.new 
-page = agent.get('http://google.com/')
-google_form = page.form('f')
-google_form.q = 'ruby mechanize'
-page = agent.submit(google_form)
-pp page
+page = agent.get('https://hh.ru/search/vacancy?area=113&st=searchVacancy&text=ruby+on+rails&customDomain=1')
+
+page = agent.page.link_with(:text => 'дальше').click
+vacancy = page.search(".HH-LinkModifier").text
+
+page.each do 
+  puts "#{vacancy}"
+  puts "==========="
+end
+
+
+
