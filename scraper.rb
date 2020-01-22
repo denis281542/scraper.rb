0c0de37475462d30853723caf8b9960486aebde9
denis281542 <deniskuimov@mail.ru>
2020-01-22 23:08:22 +0500

    Edit scraper

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 require 'mechanize'
 
 agent = Mechanize.new
-agent.pluggable_parser.default = Mechanize::Download
+agent.pluggable_parser.pdf = Mechanize::FileSaver
 agent.get('https://pravoslavnoe-duhovenstvo.ru/library/material/661/').save('save_site')
+
