3701207a4e1fe9ba8334b1d4e67946a3bd515a93
denis281542 <deniskuimov@mail.ru>
2020-01-23 11:17:59 +0500

    Edit commit

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,6 @@
 require 'mechanize'
 
 agent = Mechanize.new
-agent.pluggable_parser.pdf = Mechanize::FileSaver4
+agent.pluggable_parser.pdf = Mechanize::FileSaver
 agent.get('https://pravoslavnoe-duhovenstvo.ru/media/priestdb/materialattachment/attachment/25/5a/255a7e0c-4a45-44b1-96c2-1344636742e3.pdf').save('save_site1')
 
