5f22bfd065b8ebbd2a4f10873cda10bffd54ba03
denis281542 <deniskuimov@mail.ru>
2020-01-24 20:44:49 +0500

    Edid json.rb

--- json.rb
+++ json.rb
@@ -2,3 +2,19 @@ require 'json'
   my_object = { :array => [1, 2, 3, { :sample => "hash"} ], :foo => "bar" }
 puts JSON.pretty_generate(my_object)
 
+class PrettyJsonResponse
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    status, headers, response = @app.call(env)
+    if headers["Content-Type"] =~ /^application\/json/
+      obj = JSON.parse(response.body)
+      pretty_str = JSON.pretty_unparse(obj)
+      response = [pretty_str]
+      headers["Content-Length"] = pretty_str.bytesize.to_s
+    end
+    [status, headers, response]
+  end
+end
