92400332202d2d776baff8cded49ebe21f981e74
denis281542 <deniskuimov@mail.ru>
2020-03-04 23:30:33 +0500

    added file

--- parser.rb
+++ parser.rb
@@ -3,7 +3,7 @@ require 'faraday'
 require 'nokogiri'
 require 'open-uri'
 
-conn = Faraday.new(:url => 'http://sushi.com') do |faraday|
+conn = Faraday.new(:url => 'https://twitter.com/elonmusk') do |faraday|
   faraday.request  :url_encoded             # form-encode POST params
   faraday.response :logger                  # log requests to STDOUT
   faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
