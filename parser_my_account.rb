d8f308bad6273bfe9faaa1038edfacf84e2a8d0f
denis281542 <deniskuimov@mail.ru>
2020-03-04 23:29:03 +0500

    added file

--- parser_my_account.rb
+++ parser_my_account.rb
@@ -3,7 +3,7 @@ require 'faraday'
 require 'nokogiri'
 require 'open-uri'
 
-url = "https://twitter.com/elonmusk"
+url = "https://twitter.com/elonmusk/status/1234764390907437057/likes"
 #html = open(response)
 doc = Nokogiri::HTML(open(url))
-puts doc.css('//div//p')
+puts doc.css('.r-1j63xyz')
