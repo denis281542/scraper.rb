52ccf0cc38add65260cfb2239e1330e6050546c5
denis281542 <deniskuimov@mail.ru>
2020-03-06 14:14:34 +0500

    added file

--- parser.rb
+++ parser.rb
@@ -1,10 +1,20 @@
 require 'rubygems'
 require 'faraday'
 require 'nokogiri'
-require 'open-uri'
 
-conn = Faraday.new(:url => 'https://twitter.com/elonmusk') do |faraday|
+conn = Faraday.new(:url => 'https://twitter.com/elonmusk/') do |faraday|
   faraday.request  :url_encoded             # form-encode POST params
   faraday.response :logger                  # log requests to STDOUT
   faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP  
 end
+
+response = conn.get '/elonmusk'    # GET http://sushi.com/nigiri/sake.json
+response.body
+
+@arr = []
+response.search("@#{[:twitter_acc]}", result_type: "recent").take(params[:number_of_tweets].to_i).collect do |tweet|
+  @arr << tweet.text.gsub("@#{params[:twitter_acc]}", "").gsub("RT :", "").gsub("RT", "")
+end
+
+puts @arr
+
